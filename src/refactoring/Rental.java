55103d70b957b64a6a4832aca0daf9611e58c07b
henningxtro <henning.sextro@gmail.com>
2021-05-09 22:25:10 +0200

    Moved getFrequentRenterPoints functionality from Customer to Rental Class

--- src/refactoring/Rental.java
+++ src/refactoring/Rental.java
@@ -33,4 +33,12 @@ class Rental {
         }
         return result;
     }
+    int getFrequentRenterPoints() {
+        if ((movie.getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
 }
