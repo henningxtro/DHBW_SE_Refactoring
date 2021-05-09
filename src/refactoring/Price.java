755f9571958a30ab8369d379a27b2d1309202e26
henningxtro <henning.sextro@gmail.com>
2021-05-10 01:15:48 +0200

    Moved getFrequentRenterPoints to Price Class and used an overriding method to break up switch statement

--- src/refactoring/Price.java
+++ src/refactoring/Price.java
@@ -4,11 +4,7 @@ abstract class Price {
     abstract int getPriceCode();
     abstract double getCharge(int daysRented);
     int getFrequentRenterPoints(int daysRented) {
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return 1;
     }
 }
 
@@ -16,6 +12,7 @@ class ChildrensPrice extends Price {
     int getPriceCode() {
         return Movie.CHILDRENS;
     }
+
     double getCharge(int daysRented) {
         double result  = 1.5;
         if (daysRented > 3){
@@ -29,8 +26,13 @@ class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
     double getCharge(int daysRented) {
         return daysRented * 3;
+    }
+
+    int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2: 1;
     }
 }
 
@@ -38,6 +40,7 @@ class RegularPrice extends Price {
     int getPriceCode() {
         return Movie.REGULAR;
     }
+
     double getCharge(int daysRented) {
         double result = 2;
         if (daysRented > 2) {
